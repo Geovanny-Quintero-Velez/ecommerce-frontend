6b62891e6dc2f498313b1200872e6343a00d01c0
Juank114Gonzalez <juank123ganzalez@gmail.com>
2024-06-19 14:26:37 -0500

    fix: Some dependency errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import GridView from "./components/GridView";
-import RootLayout from "./layout";
+
+import Navbar from "./components/Navbar";
 
 export default function HomePage() {
   return (
-    <div className="mt-8">
-    <h1 className="text-2xl font-bold mb-4">Bienvenido a Mi Tienda Online</h1>
-    <p className="text-lg mb-4">Descubre nuestros productos destacados:</p>
-    <GridView /> {/* Renderiza el componente GridView aquí */}
-  </div>
+    <section>
+      <Navbar />
+      <div className="mt-8">
+        <h1 className="text-2xl font-bold mb-4">
+          Bienvenido a Mi Tienda Online
+        </h1>
+        <p className="text-lg mb-4">Descubre nuestros productos destacados:</p>
+        <GridView /> {/* Renderiza el componente GridView aquí */}
+      </div>
+    </section>
   );
 }
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -3,9 +3,9 @@ import Footer from "../components/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Login",
-    description: "This is the login page of the site.",
-  };
+  title: "Login",
+  description: "This is the login page of the site.",
+};
 
 export default function UserLayout({
   children,
@@ -14,18 +14,23 @@ export default function UserLayout({
 }>) {
   return (
     <section className="flex flex-col min-h-screen">
-      <div className=" h-screen flex items-center justify-center" style={{background: 'var(--color-background)'}}>
+      <div
+        className=" h-screen flex items-center justify-center"
+        style={{ background: "var(--color-background)" }}
+      >
         {/* Container of the whole section */}
         <div className="flex w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden">
-          
           {/* Left side */}
-          <div className="w-1/2  p-10 flex items-center justify-center" style={{background: 'var(--color-background)'}}>
-            <div style={{ color: 'var(--color-primary)'}}>
+          <div
+            className="w-1/2  p-10 flex items-center justify-center"
+            style={{ background: "var(--color-background)" }}
+          >
+            <div style={{ color: "var(--color-primary)" }}>
               <h1 className="text-5xl font-bold">Hello!</h1>
               <h1 className="text-5xl font-bold mt-2">Nice to see you!</h1>
             </div>
           </div>
-        {children}
+          {children}
         </div>
       </div>
       <Footer />
